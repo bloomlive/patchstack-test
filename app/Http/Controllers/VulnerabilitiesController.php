3722da1a36e286e1e48eca73f95e49246ff16a00
Daniel Sepp <seppdaniel@gmail.com>
2022-01-10 14:59:52 +0200

    Add index, create, show, edit and delete basic logic

--- app/Http/Controllers/VulnerabilitiesController.php
+++ app/Http/Controllers/VulnerabilitiesController.php
@@ -9,12 +9,14 @@ class VulnerabilitiesController extends Controller
 {
     public function index()
     {
-        //
+        return view('vulnerabilities.index', [
+            'vulnerabilities' => Vulnerability::query()->paginate(10)
+        ]);
     }
 
     public function create()
     {
-        //
+        return view('vulnerabilities.create');
     }
 
     public function store(Request $request)
@@ -24,12 +26,16 @@ class VulnerabilitiesController extends Controller
 
     public function show(Vulnerability $vulnerability)
     {
-        //
+        return view('vulnerabilities.show', [
+            'vulnerability' => $vulnerability
+        ]);
     }
 
     public function edit(Vulnerability $vulnerability)
     {
-        //
+        return view('vulnerabilities.edit', [
+            'vulnerability' => $vulnerability
+        ]);
     }
 
     public function update(Request $request, Vulnerability $vulnerability)
@@ -39,6 +45,10 @@ class VulnerabilitiesController extends Controller
 
     public function destroy(Vulnerability $vulnerability)
     {
-        //
+        if (!$vulnerability->delete()) {
+            return back()->with('error', __('vulnerability.delete.failed'));
+        };
+
+        return $this->index()->with('success', __('vulnerability.deleted'));
     }
 }
