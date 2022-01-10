81c24f11834efae2acf3e44d75fc53228083669e
Daniel Sepp <seppdaniel@gmail.com>
2022-01-10 11:56:47 +0200

    Add stubs for Vulnerability model, controller, migration and seeder

---
+++ app/Http/Controllers/VulnerabilitiesController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Vulnerability;
+use Illuminate\Http\Request;
+
+class VulnerabilitiesController extends Controller
+{
+    public function index()
+    {
+        //
+    }
+
+    public function create()
+    {
+        //
+    }
+
+    public function store(Request $request)
+    {
+        //
+    }
+
+    public function show(Vulnerability $vulnerability)
+    {
+        //
+    }
+
+    public function edit(Vulnerability $vulnerability)
+    {
+        //
+    }
+
+    public function update(Request $request, Vulnerability $vulnerability)
+    {
+        //
+    }
+
+    public function destroy(Vulnerability $vulnerability)
+    {
+        //
+    }
+}
